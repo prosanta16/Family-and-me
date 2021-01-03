e98f7fdb0aa4b0e5ae018c37a54e92ec6f710e1a
Prosanta Paul <prosantapaulbsmrstu@gmail.com>
2021-01-03 12:00:57 +0600

    css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import './assets/css/styles.css'
+import './assets/css/bootstrap.css'
 
 Vue.config.productionTip = false
 
